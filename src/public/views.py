b3e9dbf8d2e7625d913f0041a10f489bc28fe065
Andy Craze <accraze@gmail.com>
2020-11-03 06:45:31 -0800

    add pagination to grid view

--- src/public/views.py
+++ src/public/views.py
@@ -9,6 +9,7 @@ from flask import (
     request,
     url_for,
 )
+from flask_paginate import Pagination, get_page_args
 
 from ..models import AudioFile
 
@@ -18,11 +19,24 @@ blueprint = Blueprint("public", __name__, static_folder="../static")
 
 @blueprint.route('/', methods=["GET", "POST"])
 def grid_view():
-    name = request.args.get("name", "World")
     audio_samples = AudioFile.query.all()
-    audio_grid = [audio_samples[i * 3:(i + 1) * 3]
-                  for i in range((len(audio_samples) + 3 - 1) // 3)]
-    return render_template('base.html', grid=audio_grid)
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+    total = len(audio_samples)
+    pagination_samples = get_paginated_samples(
+        audio_samples, offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bulma')
+    audio_grid = [pagination_samples[i * 3:(i + 1) * 3]
+                  for i in range((len(pagination_samples) + 3 - 1) // 3)]
+
+    return render_template('base.html', grid=audio_grid, page=page,
+                           per_page=per_page,
+                           pagination=pagination,)
+
+
+def get_paginated_samples(samples, offset=0, per_page=9):
+    return samples[offset: offset + per_page]
 
 
 @blueprint.route('/sample/<file_name>')
