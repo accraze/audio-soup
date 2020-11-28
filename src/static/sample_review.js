7c34d7424f8207f988b6b95d497494a4f8b5f2c9
Andy Craze <accraze@gmail.com>
2020-11-28 06:38:53 -0800

    fix sizing issues on metadata form

--- src/static/sample_review.js
+++ src/static/sample_review.js
@@ -1,11 +1,11 @@
 function toggleMetadataForm(e, sampleId) {
 	$(e).attr('disabled', true)
-  document.getElementById("modal-card2_"+sampleId+"-metadata-form").style.visibility = 'visible';
-  document.getElementById("modal-card2_"+sampleId+"-metadata").style.visibility = 'hidden';
+  document.getElementById("modal-card2_"+sampleId+"-metadata-form").style.display = 'inline';
+  document.getElementById("modal-card2_"+sampleId+"-metadata").style.display = 'none';
 }
 
 function cancelMetadataForm(sampleId) {
   document.getElementById("modal-card2_"+sampleId+"-edit-btn").disabled = false;
-  document.getElementById("modal-card2_"+sampleId+"-metadata-form").style.visibility = 'hidden';
-  document.getElementById("modal-card2_"+sampleId+"-metadata").style.visibility = 'visible';
+  document.getElementById("modal-card2_"+sampleId+"-metadata-form").style.display = 'none';
+  document.getElementById("modal-card2_"+sampleId+"-metadata").style.display = 'inline';
 }
