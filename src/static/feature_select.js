28bb3c03be41c9e911c977b1191cae239c1c1e93
Andy Craze <accraze@gmail.com>
2020-11-27 21:50:26 -0800

    strip queryparams when navigating feature type

--- src/static/feature_select.js
+++ src/static/feature_select.js
@@ -7,13 +7,17 @@ function loadSpectralFeatures(url) {
 }
 
 function loadRhythmicFeatures(url) {
-	console.log('load rhythmic features!!!');
+	if (location.href.includes('?')) {
+		    history.pushState({}, null, location.href.split('?')[0]);
+	}
 	url= location.href+'?feature_type=rhythmic';
-	console.log(url);
 	location.href = url;
 }
 
 function loadDeltaFeatures(url) {
+	if (location.href.includes('?')) {
+		    history.pushState({}, null, location.href.split('?')[0]);
+	}
 	url= location.href+'?feature_type=deltas';
 	location.href = url;
 
